d68d74b2242ac501517a5e69570b771fa8d3f7cb
Arjan Schrijver <a_gadgetbridge@anymore.nl>
2024-09-09 20:54:06 +0200

    Moyoung: Support the Misirun C17

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/DeviceType.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/DeviceType.java
@@ -245,6 +245,7 @@ import nodomain.freeyourgadget.gadgetbridge.devices.miscale.MiCompositionScaleCo
 import nodomain.freeyourgadget.gadgetbridge.devices.miscale.MiSmartScaleCoordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.moondrop.MoondropSpaceTravelCoordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.moyoung.ColmiI28UltraCoordinator;
+import nodomain.freeyourgadget.gadgetbridge.devices.moyoung.MisirunC17Coordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.no1f1.No1F1Coordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.nothing.CmfBudsPro2Coordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.nothing.Ear1Coordinator;
@@ -626,6 +627,7 @@ public enum DeviceType {
     COLMI_R09(ColmiR09Coordinator.class),
     COLMI_R10(ColmiR10Coordinator.class),
     COLMI_I28_ULTRA(ColmiI28UltraCoordinator.class),
+    MISIRUN_C17(MisirunC17Coordinator.class),
     B_AND_W_P_SERIES(BandWPSeriesDeviceCoordinator.class),
     EARFUN_AIR_S(EarFunAirSCoordinator.class),
     EARFUN_AIR_PRO_4(EarFunAirPro4Coordinator.class),
