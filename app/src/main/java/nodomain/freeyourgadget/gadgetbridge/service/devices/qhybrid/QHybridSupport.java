6785c5397c328107127068820e74d612484f4ec8
carm87 <carm87@noreply.codeberg.org>
2025-05-02 20:38:29 +0000

    Aggiorna app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/qhybrid/QHybridSupport.java

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/qhybrid/QHybridSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/qhybrid/QHybridSupport.java
@@ -742,11 +742,6 @@ public class QHybridSupport extends QHybridBaseSupport {
     }
 
     @Override
-    public void handleGBDeviceEvent(GBDeviceEventBatteryInfo deviceEvent){
-        super.handleGBDeviceEvent(deviceEvent);
-    }
-
-    @Override
     public boolean onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
         watchAdapter.onCharacteristicWrite(gatt, characteristic, status);
         return super.onCharacteristicWrite(gatt, characteristic, status);
