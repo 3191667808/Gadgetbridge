ddb200541b5d2aec789e1a6e03a2e90f7c279990
Thomas Kuehne <thomas.kuehne@gmx.li>
2025-06-28 00:00:00 +0200

    ble: BtLEQueue optimizations for CompanionDevice auto connect (#5019)

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/xiaomi/XiaomiBleSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/xiaomi/XiaomiBleSupport.java
@@ -224,8 +224,10 @@ public class XiaomiBleSupport extends XiaomiConnectionSupport {
 
         @Override
         public void dispose() {
-            mXiaomiSupport.onDisconnect();
-            super.dispose();
+            synchronized (ConnectionMonitor) {
+                mXiaomiSupport.onDisconnect();
+                super.dispose();
+            }
         }
     };
 
