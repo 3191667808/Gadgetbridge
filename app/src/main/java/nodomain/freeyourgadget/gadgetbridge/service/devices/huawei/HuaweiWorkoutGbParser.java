4d1e4ab08addc61defbee857c3a52c2f03db2597
Adrià <abrucort@ik.me>
2025-07-11 23:19:08 +0200

    remove distinction between recreational scuba and tech scuba

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/huawei/HuaweiWorkoutGbParser.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/huawei/HuaweiWorkoutGbParser.java
@@ -130,8 +130,11 @@ public class HuaweiWorkoutGbParser implements ActivitySummaryParser {
         HIKING(14, ActivityKind.HIKING),
         JUMP_ROPING(21, ActivityKind.JUMP_ROPING),
         FREE_DIVING(22, ActivityKind.FREE_DIVING),
+<<<<<<< HEAD
         APNEA_TRAINING(23, ActivityKind.APNEA_TRAINING),
         APNEA_TEST(24, ActivityKind.APNEA_TEST),
+=======
+>>>>>>> 2bf500dde (remove distinction between recreational scuba and tech scuba)
         SCUBA_DIVING(25, ActivityKind.SCUBA_DIVING),
         PING_PONG(128, ActivityKind.PINGPONG),
         BADMINTON(129, ActivityKind.BADMINTON),
