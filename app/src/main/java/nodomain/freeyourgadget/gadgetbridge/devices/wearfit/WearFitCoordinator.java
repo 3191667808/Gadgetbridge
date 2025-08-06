4f86cf21b690ce6f61e0cb8bc01b094d77ecfa58
Vitaliy Tomin <highwaystar.ru@gmail.com>
2025-08-06 23:13:12 +0800

    wearfit: Use HK.* mask for supported devices

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/wearfit/WearFitCoordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/wearfit/WearFitCoordinator.java
@@ -149,7 +149,7 @@ public class WearFitCoordinator extends AbstractBLEDeviceCoordinator {
 
     @Override
     protected Pattern getSupportedDeviceName() {
-        return Pattern.compile("HK8.*");
+        return Pattern.compile("HK.*");
     }
 
     @Override
