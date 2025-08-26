b4955d6898ab20ae5f8713c36abe2af438d1444f
gfi93a <gfi93a@noreply.codeberg.org>
2025-08-26 22:19:14 +0200

    fixed migration version

--- GBDaoGenerator/src/nodomain/freeyourgadget/gadgetbridge/daogen/GBDaoGenerator.java
+++ GBDaoGenerator/src/nodomain/freeyourgadget/gadgetbridge/daogen/GBDaoGenerator.java
@@ -58,7 +58,7 @@ public class GBDaoGenerator {
 
 
     public static void main(String[] args) throws Exception {
-        final Schema schema = new Schema(117, MAIN_PACKAGE + ".entities");
+        final Schema schema = new Schema(119, MAIN_PACKAGE + ".entities");
 
         Entity userAttributes = addUserAttributes(schema);
         Entity user = addUserInfo(schema, userAttributes);
