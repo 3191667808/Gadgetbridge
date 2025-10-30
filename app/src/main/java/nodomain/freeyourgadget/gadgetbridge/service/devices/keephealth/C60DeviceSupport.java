9a8463679adf9163c3a931f7491340df1c1c1261
LiJu09 <codeberg@liju09.emajl.sk>
2025-10-30 21:20:14 +0100

    keephealth: C60 add more commands

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/keephealth/C60DeviceSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/keephealth/C60DeviceSupport.java
@@ -28,6 +28,34 @@ public class C60DeviceSupport extends AbstractBTLESingleDeviceSupport {
     private final byte[] CMD_GET_CURRENT_BATTERY = { 0x27, 0x00, 0x00, 0x74 };
     private final byte[] CMD_GET_DEVICE_STATE = { 0x02, 0x00, 0x00, 0x06 };
     private final byte[] CMD_GET_CURRENT_STEPS = { 0x20, 0x01, 0x00, 0x00, 0x70 };
+    private final byte[] CMD_GET_CURRENT_HEARTRATE = { 0x21, 0x01, 0x00, 0x00, (byte) 0xc6 };
+    private final byte[] CMD_GET_CURRENT_BODYTEMP = { 0x2c, 0x01, 0x00, 0x00, (byte) 0x78 };
+    private final byte[] CMD_SET_DEVICE_STATE = { // TODO build based on settings
+            (byte) 0x02, (byte) 0x10, (byte) 0x00, (byte) 0x64, (byte) 0x05,
+            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x01,
+            (byte) 0x00, (byte) 0x00, (byte) 0xFF, (byte) 0x00, (byte) 0x00,
+            (byte) 0x00, (byte) 0x00, (byte) 0x02, (byte) 0x00, (byte) 0x58
+    };
+    private final byte[] CMD_SET_USER_INFO = { // TODO build based on settings
+            (byte) 0x03, (byte) 0x07, (byte) 0x00, (byte) 0x00, (byte) 0x14,
+            (byte) 0xAA, (byte) 0x00, (byte) 0x58, (byte) 0x02, (byte) 0x46,
+            (byte) 0x4A
+    };
+    private final byte[] CMD_GET_TARGET_DATA = { 0x07, 0x00, 0x00, (byte) 0xb4 };
+    private final byte[] CMD_GET_NOTICE = { 0x09, 0x00, 0x00, (byte) 0x60 }; // TODO find more about
+
+    // Obtain blood pressure and blood oxygen data
+    private final byte[] CMD_GET_OXYGEN = { 0x21, 0x01, 0x00, 0x07, (byte) 0x20 }; // TODO find more about
+
+    // Obtaining automatic heart rate sampling data
+    private final byte[] CMD_GET_HEARTRATE_SAMPLING = { 0x21, 0x01, 0x00, 0x08, (byte) 0x76 }; // TODO find more about
+
+    // two fragment response
+    private final byte[] CMD_GET_ALARM = { 0x02, 0x00, 0x08, (byte) 0x80 }; // TODO find more about
+
+    // Step count and sleep history data, 15 fragment response
+    private final byte[] CMD_GET_CURRENT_HISTORY_STEP = { 0x20, 0x05, 0x00, 0x01 };
+
 
     public C60DeviceSupport() {
         super(LOG);
@@ -48,6 +76,26 @@ public class C60DeviceSupport extends AbstractBTLESingleDeviceSupport {
         getDeviceState(builder);
         builder.wait(200);
         getSteps(builder);
+        builder.wait(200);
+        getHeartrate(builder);
+        builder.wait(200);
+        getBodytemp(builder);
+        builder.wait(200);
+        setDeviceState(builder);
+        builder.wait(200);
+        setUserInfo(builder);
+        builder.wait(200);
+        getTargetData(builder);
+        builder.wait(200);
+        getNotice(builder);
+        builder.wait(200);
+        getOxygen(builder);
+        builder.wait(200);
+        getHrSampling(builder);
+        builder.wait(200);
+        getAlarm(builder);
+        builder.wait(200);
+        getStepsHistory(builder);
         builder.wait(200);
 
         getDevice().setFirmwareVersion("N/A");
@@ -91,6 +139,8 @@ public class C60DeviceSupport extends AbstractBTLESingleDeviceSupport {
                 handleDeviceState(value);
             } else if (cmdPrefix == CMD_GET_CURRENT_STEPS[0]) {
                 handleSteps(value);
+            } else {
+                LOG.info("Unhandled data: {}", GB.hexdump(value));
             }
         } else {
             LOG.info("Received data have invalid checksum: {}", GB.hexdump(value));
@@ -149,7 +199,7 @@ public class C60DeviceSupport extends AbstractBTLESingleDeviceSupport {
         int low = year & 0xFF;
         buf.put((byte) low);
         buf.put((byte) high);
-        buf.put((byte) calendar.get(Calendar.MONTH));
+        buf.put((byte) calendar.get(Calendar.MONTH + 1));
         buf.put((byte) calendar.get(Calendar.DAY_OF_MONTH));
         buf.put((byte) calendar.get(Calendar.HOUR_OF_DAY));
         buf.put((byte) calendar.get(Calendar.MINUTE));
@@ -181,6 +231,70 @@ public class C60DeviceSupport extends AbstractBTLESingleDeviceSupport {
 
     public C60DeviceSupport getSteps(TransactionBuilder builder) {
         builder.write(C60Constants.CHARACTERISTIC_WRITE, CMD_GET_CURRENT_STEPS);
+        return this;
+    }
+
+    public C60DeviceSupport getStepsHistory(TransactionBuilder builder) {
+        byte length = 9;
+        ByteBuffer buf = ByteBuffer.allocate(length);
+        buf.order(ByteOrder.LITTLE_ENDIAN);
+
+        buf.put(CMD_GET_CURRENT_HISTORY_STEP);
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int high = (year >> 8) & 0xFF;
+        int low = year & 0xFF;
+        buf.put((byte) low);
+        buf.put((byte) high);
+        buf.put((byte) (calendar.get(Calendar.MONTH) + 1));
+        buf.put((byte) calendar.get(Calendar.DAY_OF_MONTH));
+        buf.put(getChecksum(buf.array()));
+        builder.write(C60Constants.CHARACTERISTIC_WRITE, buf.array());
+        return this;
+    }
+
+    public C60DeviceSupport getHeartrate(TransactionBuilder builder) {
+        builder.write(C60Constants.CHARACTERISTIC_WRITE, CMD_GET_CURRENT_HEARTRATE);
+        return this;
+    }
+
+    public C60DeviceSupport getBodytemp(TransactionBuilder builder) {
+        builder.write(C60Constants.CHARACTERISTIC_WRITE, CMD_GET_CURRENT_BODYTEMP);
+        return this;
+    }
+
+    public C60DeviceSupport setDeviceState(TransactionBuilder builder) {
+        builder.write(C60Constants.CHARACTERISTIC_WRITE, CMD_SET_DEVICE_STATE);
+        return this;
+    }
+
+    public C60DeviceSupport setUserInfo(TransactionBuilder builder) {
+        builder.write(C60Constants.CHARACTERISTIC_WRITE, CMD_SET_USER_INFO);
+        return this;
+    }
+
+    public C60DeviceSupport getTargetData(TransactionBuilder builder) {
+        builder.write(C60Constants.CHARACTERISTIC_WRITE, CMD_GET_TARGET_DATA);
+        return this;
+    }
+
+    public C60DeviceSupport getNotice(TransactionBuilder builder) {
+        builder.write(C60Constants.CHARACTERISTIC_WRITE, CMD_GET_NOTICE);
+        return this;
+    }
+
+    public C60DeviceSupport getOxygen(TransactionBuilder builder) {
+        builder.write(C60Constants.CHARACTERISTIC_WRITE, CMD_GET_OXYGEN);
+        return this;
+    }
+
+    public C60DeviceSupport getHrSampling(TransactionBuilder builder) {
+        builder.write(C60Constants.CHARACTERISTIC_WRITE, CMD_GET_HEARTRATE_SAMPLING);
+        return this;
+    }
+
+    public C60DeviceSupport getAlarm(TransactionBuilder builder) {
+        builder.write(C60Constants.CHARACTERISTIC_WRITE, CMD_GET_ALARM);
         return this;
     }
 
