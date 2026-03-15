3d47086345c4a8f894a7b29d6c149fb969cf9894
Thomas Kuehne <thomas.kuehne@gmx.li>
2026-03-15 14:06:44 +0100

    Garmin: GpxRouteFileConverter add depth, temperature, speed and tests

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/GarminSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/GarminSupport.java
@@ -1328,7 +1328,7 @@ public class GarminSupport extends AbstractBTLESingleDeviceSupport implements IC
             final String trackName = options.getString(GarminGpxRouteInstallHandler.EXTRA_TRACK_NAME);
             final GpxRouteFileConverter gpxRouteFileConverter = new GpxRouteFileConverter(
                     garminGpxRouteInstallHandler.getGpxFile(),
-                    trackName
+                    trackName, null
             );
             final FitFile convertedFile = gpxRouteFileConverter.getConvertedFile();
             final FileType.FILETYPE fileType = convertedFile.getFileType();
