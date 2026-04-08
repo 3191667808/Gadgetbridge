34f44d076c3bb0046fe3c79b8b43e3e288839c58
duhow <contacto@duhowpi.net>
2026-04-08 23:45:15 +0200

    refactor equalizer data

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/soundcore/sport_x20/SoundcoreSportX20Protocol.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/soundcore/sport_x20/SoundcoreSportX20Protocol.java
@@ -32,9 +32,10 @@ public class SoundcoreSportX20Protocol extends SoundcoreLibertyProtocol {
     private static final int[] EQ_TAIL_SCALE = new int[]{5, 3, 4, 4, 4, 4, 4, 6};
     private static final int EQ_TAIL_DIVISOR = 6;
 
-    private static final byte[] EQ_CUSTOM_TEMPLATE = decodeHex(
-            "fefe00007878787878787878787878787878787878780002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c787878787878787878007878787878787878780000"
-    );
+    private static final String PRESET_CHANNEL_SEPARATOR = "00";
+    private static final String PRESET_SUFFIX = "0000";
+    private static final String PRESET_COMMON_SECTION =
+        "000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c";
 
     private static final Map<Integer, byte[]> EQ_PRESET_PAYLOADS = buildPresetPayloads();
 
@@ -251,19 +252,21 @@ public class SoundcoreSportX20Protocol extends SoundcoreLibertyProtocol {
             }
         }
 
-        final byte[] payload = EQ_CUSTOM_TEMPLATE.clone();
+        final int[] rawBands = new int[EQ_BANDS];
+        final int[] tailBands = new int[EQ_BANDS];
 
         for (int i = 0; i < EQ_BANDS; i++) {
             final int value = clamp(prefs.getInt(EQUALIZER_PREFS_VALUE[i], 0), -6, 6);
 
-            final int rawBand = 120 + (value * 10);
-            final int tailBand = 120 + Math.round((float) (value * EQ_TAIL_SCALE[i]) / EQ_TAIL_DIVISOR);
-
-            payload[4 + i] = (byte) rawBand;
-            payload[13 + i] = (byte) rawBand;
-            payload[72 + i] = (byte) tailBand;
-            payload[82 + i] = (byte) tailBand;
+            rawBands[i] = 120 + (value * 10);
+            tailBands[i] = 120 + Math.round((float) (value * EQ_TAIL_SCALE[i]) / EQ_TAIL_DIVISOR);
         }
+
+        final byte[] payload = buildPresetPayload(
+            CUSTOM_PRESET_ID,
+            bandsToHex(rawBands),
+            bandsToHex(tailBands)
+        );
 
         return new SoundcorePacket(CMD_SET_EQUALIZER, payload).encode();
     }
@@ -294,30 +297,60 @@ public class SoundcoreSportX20Protocol extends SoundcoreLibertyProtocol {
     private static Map<Integer, byte[]> buildPresetPayloads() {
         final Map<Integer, byte[]> presetPayloads = new HashMap<>();
 
-        presetPayloads.put(0x00, decodeHex("0000000078787878787878787800787878787878787878000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c787878787878787878007878787878787878780000"));
-        presetPayloads.put(0x01, decodeHex("01000000a0828c8ca0a0a08c7800a0828c8ca0a0a08c78000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c7d767b787c7a7c7978007d767b787c7a7c79780000"));
-        presetPayloads.put(0x02, decodeHex("02000000a0968278787878787800a09682787878787878000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c7b7a78787878787878007b7a787878787878780000"));
-        presetPayloads.put(0x03, decodeHex("03000000505a6e78787878787800505a6e787878787878000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c757678787878787878007576787878787878780000"));
-        presetPayloads.put(0x04, decodeHex("0400000096966464788c96a0780096966464788c96a078000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c7a7c7477787a797d78007a7c7477787a797d780000"));
-        presetPayloads.put(0x05, decodeHex("050000005a8ca0a0968c786478005a8ca0a0968c786478000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c747b7a7b797a78757800747b7a7b797a7875780000"));
-        presetPayloads.put(0x06, decodeHex("060000008c5a6e828c8c825a78008c5a6e828c8c825a78000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c7c7378787a797b7378007c7378787a797b73780000"));
-        presetPayloads.put(0x07, decodeHex("070000008c8296968c64504678008c8296968c64504678000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c7a777b797b76767378007a777b797b767673780000"));
-        presetPayloads.put(0x08, decodeHex("08000000968c648c828c96967800968c648c828c969678000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c7a7b737d777a7a7b78007a7b737d777a7a7b780000"));
-        presetPayloads.put(0x09, decodeHex("0900000064646e7878786464780064646e787878646478000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c767777787879767678007677777878797676780000"));
-        presetPayloads.put(0x0a, decodeHex("0a0000008c966e6e8c6e8c9678008c966e6e8c6e8c9678000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c797c76767d747b7b7800797c76767d747b7b780000"));
-        presetPayloads.put(0x0b, decodeHex("0b0000008c8c6464788c96a078008c8c6464788c96a078000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c797b7577787a797d7800797b7577787a797d780000"));
-        presetPayloads.put(0x0c, decodeHex("0c00000078786464647896aa780078786464647896aa78000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c7879767776787a7e78007879767776787a7e780000"));
-        presetPayloads.put(0x0d, decodeHex("0d0000006e8ca09678648c8278006e8ca09678648c8278000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c767a7b7a78747c787800767a7b7a78747c78780000"));
-        presetPayloads.put(0x0e, decodeHex("0e0000007896968ca0aa96a078007896968ca0aa96a078000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c777b7a787b7c787d7800777b7a787b7c787d780000"));
-        presetPayloads.put(0x0f, decodeHex("0f0000006e829696826e645a78006e829696826e645a78000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c77797a7a79777775780077797a7a79777775780000"));
-        presetPayloads.put(0x10, decodeHex("10000000b48c64648c9696a07800b48c64648c9696a078000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c7e7976757b7a797d78007e7976757b7a797d780000"));
-        presetPayloads.put(0x11, decodeHex("11000000968c6e6e8296a0aa7800968c6e6e8296a0aa78000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c7a7a7677797a7a7e78007a7a7677797a7a7e780000"));
-        presetPayloads.put(0x12, decodeHex("12000000a0968278645a50507800a0968278645a505078000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c7b7a78797676757478007b7a787976767574780000"));
-        presetPayloads.put(0x13, decodeHex("130000005a64828c8c82785a78005a64828c8c82785a78000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c76767a797a787974780076767a797a787974780000"));
-        presetPayloads.put(0x14, decodeHex("140000006464646e828c8ca078006464646e828c8ca078000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c76777777797a787d780076777777797a787d780000"));
-        presetPayloads.put(0x15, decodeHex("15000000787878645a50503c7800787878645a50503c78000002009fa18a998a8077643c3c9fa18a998a8077643c3c69d64e6a009fa18a998a8077643c3c9fa18a998a8077643c3c787879767775777178007878797677757771780000"));
+        presetPayloads.put(0x00, buildPresetPayload(0x00, "787878787878787878", "787878787878787878"));
+        presetPayloads.put(0x01, buildPresetPayload(0x01, "a0828c8ca0a0a08c78", "7d767b787c7a7c7978"));
+        presetPayloads.put(0x02, buildPresetPayload(0x02, "a09682787878787878", "7b7a78787878787878"));
+        presetPayloads.put(0x03, buildPresetPayload(0x03, "505a6e787878787878", "757678787878787878"));
+        presetPayloads.put(0x04, buildPresetPayload(0x04, "96966464788c96a078", "7a7c7477787a797d78"));
+        presetPayloads.put(0x05, buildPresetPayload(0x05, "5a8ca0a0968c786478", "747b7a7b797a787578"));
+        presetPayloads.put(0x06, buildPresetPayload(0x06, "8c5a6e828c8c825a78", "7c7378787a797b7378"));
+        presetPayloads.put(0x07, buildPresetPayload(0x07, "8c8296968c64504678", "7a777b797b76767378"));
+        presetPayloads.put(0x08, buildPresetPayload(0x08, "968c648c828c969678", "7a7b737d777a7a7b78"));
+        presetPayloads.put(0x09, buildPresetPayload(0x09, "64646e787878646478", "767777787879767678"));
+        presetPayloads.put(0x0a, buildPresetPayload(0x0a, "8c966e6e8c6e8c9678", "797c76767d747b7b78"));
+        presetPayloads.put(0x0b, buildPresetPayload(0x0b, "8c8c6464788c96a078", "797b7577787a797d78"));
+        presetPayloads.put(0x0c, buildPresetPayload(0x0c, "78786464647896aa78", "7879767776787a7e78"));
+        presetPayloads.put(0x0d, buildPresetPayload(0x0d, "6e8ca09678648c8278", "767a7b7a78747c7878"));
+        presetPayloads.put(0x0e, buildPresetPayload(0x0e, "7896968ca0aa96a078", "777b7a787b7c787d78"));
+        presetPayloads.put(0x0f, buildPresetPayload(0x0f, "6e829696826e645a78", "77797a7a7977777578"));
+        presetPayloads.put(0x10, buildPresetPayload(0x10, "b48c64648c9696a078", "7e7976757b7a797d78"));
+        presetPayloads.put(0x11, buildPresetPayload(0x11, "968c6e6e8296a0aa78", "7a7a7677797a7a7e78"));
+        presetPayloads.put(0x12, buildPresetPayload(0x12, "a0968278645a505078", "7b7a78797676757478"));
+        presetPayloads.put(0x13, buildPresetPayload(0x13, "5a64828c8c82785a78", "76767a797a78797478"));
+        presetPayloads.put(0x14, buildPresetPayload(0x14, "6464646e828c8ca078", "76777777797a787d78"));
+        presetPayloads.put(0x15, buildPresetPayload(0x15, "787878645a50503c78", "787879767775777178"));
 
         return presetPayloads;
+    }
+
+    private static byte[] buildPresetPayload(final int presetId, final String bands, final String tails) {
+        return decodeHex(
+                toHexByte(presetId)
+                        + "000000"
+                        + bands
+                        + PRESET_CHANNEL_SEPARATOR
+                        + bands
+                        + PRESET_COMMON_SECTION
+                        + tails
+                        + PRESET_CHANNEL_SEPARATOR
+                        + tails
+                        + PRESET_SUFFIX
+        );
+    }
+
+    private static String bandsToHex(final int[] bands) {
+        final StringBuilder sb = new StringBuilder(bands.length * 2);
+
+        for (final int band : bands) {
+            sb.append(toHexByte(band));
+        }
+
+        return sb.toString();
+    }
+
+    private static String toHexByte(final int value) {
+        final String hex = Integer.toHexString(value & 0xff);
+        return hex.length() < 2 ? "0" + hex : hex;
     }
 
     private static byte[] decodeHex(final String hex) {
