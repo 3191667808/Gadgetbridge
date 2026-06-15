be73683d697a3652b69664dd2d0ac48df2a26682
Thomas Kuehne <thomas.kuehne@gmx.li>
2026-06-15 18:24:40 +0000

    BtLEQueue: improve log for LLM analysis

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/btbr/AbstractBTBRDeviceSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/btbr/AbstractBTBRDeviceSupport.java
@@ -280,7 +280,7 @@ public abstract class AbstractBTBRDeviceSupport extends AbstractDeviceSupport im
     @Override
     public void onConnectionEstablished() {
         try {
-            initializeDevice(createTransactionBuilder("Initializing device")).queue();
+            initializeDevice(createTransactionBuilder("initializeDevice")).queue();
         } catch (final Exception ex) {
             final GBDevice device = getDevice();
 
