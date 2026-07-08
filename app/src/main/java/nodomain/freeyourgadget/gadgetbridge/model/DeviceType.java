6fa33c209d361c9d43f134c7ac617d0ec20be004
steve <s8weber@uwaterloo.ca>
2026-07-08 18:27:24 +0000

    Initial support for MoYoung L70
    Initial support for MoYoung L70 this and includes fixes so hartrate data works
    MoYoung also hads some issues with bad stress and blodoxygen values so filter them out.

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/DeviceType.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/DeviceType.java
@@ -377,6 +377,7 @@ import nodomain.freeyourgadget.gadgetbridge.devices.moyoung.HC96Coordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.moyoung.ITechActive3Coordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.moyoung.KT80Coordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.moyoung.KsixVentureCoordinator;
+import nodomain.freeyourgadget.gadgetbridge.devices.moyoung.L70Coordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.moyoung.MisirunC17Coordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.moyoung.R50ProCoordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.moyoung.RainbuvvyT97Coordinator;
@@ -967,6 +968,7 @@ public enum DeviceType {
     BLACKVIEW_R60(BlackviewR60Coordinator.class),
     MISIRUN_C17(MisirunC17Coordinator.class),
     C20(C20Coordinator.class),
+    L70(L70Coordinator.class),
     HC96(HC96Coordinator.class),
     KT80(KT80Coordinator.class),
     ITECH_ACTIVE_3(ITechActive3Coordinator.class),
