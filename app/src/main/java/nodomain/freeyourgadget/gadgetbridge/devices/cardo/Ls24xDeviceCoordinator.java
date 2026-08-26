029560212f00b3b8497eb0027d1fcd7878831cbf
José Rebelo <joserebelo@outlook.com>
2026-08-26 13:39:27 +0100

    Cardo Intercoms (LS2-4x): Add missing DeviceKind, update control icon

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/cardo/Ls24xDeviceCoordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/cardo/Ls24xDeviceCoordinator.java
@@ -1,21 +1,15 @@
 package nodomain.freeyourgadget.gadgetbridge.devices.cardo;
 
-import android.content.Context;
-import android.content.Intent;
-
 import androidx.annotation.NonNull;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import nodomain.freeyourgadget.gadgetbridge.GBException;
 import nodomain.freeyourgadget.gadgetbridge.R;
 import nodomain.freeyourgadget.gadgetbridge.activities.devicesettings.DeviceSpecificSettings;
 import nodomain.freeyourgadget.gadgetbridge.devices.AbstractBLEDeviceCoordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.DeviceCardAction;
-import nodomain.freeyourgadget.gadgetbridge.entities.DaoSession;
-import nodomain.freeyourgadget.gadgetbridge.entities.Device;
 import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
 import nodomain.freeyourgadget.gadgetbridge.service.DeviceSupport;
 import nodomain.freeyourgadget.gadgetbridge.service.devices.cardo.CardoDeviceSupport;
@@ -31,11 +25,6 @@ public class Ls24xDeviceCoordinator extends AbstractBLEDeviceCoordinator {
     }
 
     @Override
-    protected void deleteDevice(@NonNull GBDevice gbDevice, @NonNull Device device, @NonNull DaoSession session) throws GBException {
-
-    }
-
-    @Override
     public String getManufacturer() {
         return "Cardo";
     }
@@ -47,10 +36,9 @@ public class Ls24xDeviceCoordinator extends AbstractBLEDeviceCoordinator {
 
     @NonNull
     @Override
-    public Class<? extends DeviceSupport> getDeviceSupportClass(GBDevice device) {
+    public Class<? extends DeviceSupport> getDeviceSupportClass(@NonNull GBDevice device) {
         return CardoDeviceSupport.class;
     }
-
 
     @Override
     public DeviceSpecificSettings getDeviceSpecificSettings(final GBDevice device) {
@@ -76,25 +64,17 @@ public class Ls24xDeviceCoordinator extends AbstractBLEDeviceCoordinator {
 
     @Override
     public List<DeviceCardAction> getCustomActions() {
-        return Collections.singletonList(new Ls24xDeviceCoordinator.ControlDeviceCardAction());
+        return Collections.singletonList(
+                DeviceCardAction.forActivity(
+                        R.drawable.ic_settings_applications,
+                        R.string.remote_control,
+                        DynamicActivity.class
+                )
+        );
     }
 
-    private static final class ControlDeviceCardAction implements DeviceCardAction {
-        @Override
-        public int getIcon(GBDevice device) {
-            return R.drawable.ic_steering_wheel;
-        } //TODO Changeme
-
-        @Override
-        public String getDescription(final GBDevice device, final Context context) {
-            return context.getString(R.string.remote_control);
-        }
-
-        @Override
-        public void onClick(final GBDevice device, final Context context) {
-            final Intent startIntent = new Intent(context, DynamicActivity.class);
-            startIntent.putExtra(GBDevice.EXTRA_DEVICE, device);
-            context.startActivity(startIntent);
-        }
+    @Override
+    public DeviceKind getDeviceKind(@NonNull GBDevice device) {
+        return DeviceKind.INTERCOM;
     }
 }
